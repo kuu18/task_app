d201caaac385332dcfefd5b4b23867e3d92515c7
kuu18 <kdai18.kuu@gmail.com>
2022-01-30 18:20:04 +0900

    first commit

---
+++ src/main/java/com/example/task_app/Controller/TaskRegisterController.java
@@ -0,0 +1,15 @@
+package com.example.task_app.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/task")
+public class TaskRegisterController {
+  @RequestMapping(value = "/register")
+  public ModelAndView taskRegister(ModelAndView mav){
+    mav.setViewName("/taskinput");
+    return mav;
+  }  
+}
